bbc372b87684d5faeccbf9359a898e1ebf4fb7ad
Traian Fratean <traian.fratean@gmail.com>
2016-06-19 08:28:14 +0300

    Stop slideshow

--- content_script.js
+++ content_script.js
@@ -1,11 +1,13 @@
+var intervalId;
+
 function clickNext() {
     var next = document.getElementsByClassName("oJhm5 gMFQN")[0];
     next.click();
 
-    setInterval(function(){
+    intervalId = setInterval(function(){
                     next.click();
                 }, 3000);
-    
 }
 
 clickNext();
+intervalId
--- popup.js
+++ popup.js
@@ -1,9 +1,24 @@
+var intervalId = -1;
+
 function injectTheScript() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         // query the active tab, which will be only one tab
         //and inject the script in it
-        chrome.tabs.executeScript(tabs[0].id, {file: "content_script.js"});
+        chrome.tabs.executeScript(tabs[0].id, {file: "content_script.js"}, function (test){
+            console.log(test);
+        });
     });
-}
+};
 
-document.getElementById('clickactivity').addEventListener('click', injectTheScript);
+function startslideshow() {
+    intervalId = injectTheScript();
+};
+
+function stopslideshow() {
+    if(intervalId != -1) {
+        clearInterval(intervalId);
+    }
+};
+
+document.getElementById('startslideshow').addEventListener('click', startslideshow);
+document.getElementById('stopslideshow').addEventListener('click', stopslideshow);
